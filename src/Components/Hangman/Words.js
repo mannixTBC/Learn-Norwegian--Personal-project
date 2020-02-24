8e585e550cff4a02b6637aa137615f6c226f3951
teo <teofilbadelita@gmail.com>
2020-02-24 22:14:22 +0100

    Hangman

--- src/Components/Hangman/Words.js
+++ src/Components/Hangman/Words.js
@@ -1,10 +1,10 @@
 var programming_languages = [
-	["python","cum se numete python"],
-	["beklager","Cum spui imi pare rau?"],
-	["bil","cum se traduce in norvegiana cuvantul masina?"],
-	["mor","cum se traduce in norvegiana cuvantul mama?"],
-	["flyplassen","cum se traduce pe norvegiana aeroport?"],
-	["hus","cum se traduce pe norvegiana cuvantul casa?"],
+	
+	["beklager","Cum traduci imi pare rau?"],
+	["bil","Cum se traduce in norvegiana cuvantul masina?"],
+	["mor","Cum se traduce in norvegiana cuvantul mama?"],
+	["flyplassen","Cum se traduce pe norvegiana aeroport?"],
+	["hus","Cum se traduce pe norvegiana cuvantul casa?"],
 	
 ]
 
